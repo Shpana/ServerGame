c592a17fbfac92dbe2ce99ae60f1dd7bfbbe4e04
Roman.Kozlov <romanKozell@yandex.ru>
2020-03-01 18:02:50 +0300

    *

--- engine/__init__.py
+++ engine/__init__.py
@@ -16,6 +16,6 @@ from engine.RenderEngine.Scene          import Scene
 from engine.Utils.Matrix                import Matrix
 
 # Events
-from engine.Events.Events               import _events
+_events = dict()
 from engine.Events.EventsEmmiter        import EventsEmmiter
 from engine.Events.EventsListener       import EventsListener
